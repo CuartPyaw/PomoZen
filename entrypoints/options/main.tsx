3b1499e947d9dba88518bc749efc22866c721ec3
CuartPyaw <mxxlox9@gmail.com>
2026-03-28 16:33:49 +0800

    fix: 修复 entrypoints 入口文件路径

--- entrypoints/options/main.tsx
+++ entrypoints/options/main.tsx
@@ -3,7 +3,7 @@
  * 设置页面，包含统计数据和设置功能，不包含计时器
  */
 import { createRoot } from 'react-dom/client';
-import SettingsPage from '../SettingsPage';
+import SettingsPage from '../../SettingsPage';
 
 const SettingsApp = () => {
   return <SettingsPage />;
